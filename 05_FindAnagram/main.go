814ca7426db04ff05bec00edab80bc104bf08198
csamuro <3danger@mail,ru>
2022-05-11 09:29:30 +0300

    05 anagram: v002

--- 05_FindAnagram/main.go
+++ 05_FindAnagram/main.go
@@ -1,45 +1,32 @@
 package main
 
 import (
-	an "FindAnagram/pkg/anagram"
+	"FindAnagram/pkg/anagram"
+	ut "FindAnagram/pkg/utils"
+	"flag"
 	"fmt"
-	"strings"
 )
 
-func main() {
-	combinations := an.GetCombinations([]rune("hel"))
-	data := make([]string, len(combinations))
-	for i := range data {
-		data[i] = string(combinations[i])
-	}
-	fmt.Println(strings.Join(data, "\n"))
-	fmt.Println(len(data))
+//GetInput обрабатываем флаги
+func GetInput() (filePath string) {
+	flag.StringVar(&filePath, "f", "files/small_file.txt", "file name")
+	flag.Parse()
+	return
+}
 
-	//	123
-	//131	-
-	//	132
-	//133	-
-	//211	-
-	//212	-
-	//	213
-	//221	-
-	//222	-
-	//223	-
-	//	231
-	//232	-
-	//233	-
-	//311	-
-	//	312
-	//313	-
-	//	321
-	//322	-
-	//323	-
-	//331	-
-	//332	-
-	//333	--
-	//111	--
-	//112	-
-	//113	-
-	//121	-
-	//122	-
+func main() {
+	// Получаем данные
+	filePath := GetInput()
+	data := ut.ReadFromFile(filePath)
+
+	// <<< Функция поиска всех множеств анаграмм по словарю.>>>
+	result := anagram.FindAnagram(data)
+
+	//Выводим результат
+	for k, v := range *result {
+		fmt.Printf("Group: %s\n", k)
+		for _, v := range v {
+			fmt.Printf("\t: %s\n", v)
+		}
+	}
 }
--- 05_FindAnagram/pkg/anagram/anagram.go
+++ 05_FindAnagram/pkg/anagram/anagram.go
@@ -1,66 +1,56 @@
 package anagram
 
 import (
-	ut "FindAnagram/pkg/utils"
+	"reflect"
+	"strings"
 )
 
-func GetCombinations(word []rune) [][]rune {
-	var (
-		size    = len(word)
-		setSize = size * size
-		words   = make([][]rune, 0, setSize)
-		arri    = make([]int, size)
-	)
-
-	for j := range arri {
-		arri[j] = j
-	}
-
-	for w := 0; w < setSize; w++ {
-		words = append(words, make([]rune, size))
-		for j := range arri {
-			words[w][j] = word[arri[j]]
-		}
-		nextComb(arri, len(arri), true)
-		if ut.HasCycleCompeted(arri) {
-			break
-		}
-	}
-	return words
-}
-
-func nextComb(arri []int, size int, rec bool) {
-	lastIndex := len(arri) - 1
-	arri[lastIndex]++
-	if arri[lastIndex] == size {
-		arri[lastIndex] = 0
-		if lastIndex != 0 {
-			nextComb(arri[:lastIndex], size, false)
-		}
-	}
-	if rec && ut.HasEqual(arri) {
-		nextComb(arri, size, true)
-	}
-}
-
-func hasCycle(arri []int) bool {
-	size := len(arri)
-	if arri[size-1] == size-2 {
-		for i := range arri[:size-1] {
-			if arri[i] == i-1 {
-				return true
+// Sort сортирует буквы по алфавиту для дальнейшего использования в качестве ключа
+func Sort[T string | rune](data []T) []T {
+	size := len(data)
+	swapper := reflect.Swapper(data)
+	for j := 0; j < size; j++ {
+		for k := j + 1; k < size; k++ {
+			if data[j] > data[k] {
+				swapper(j, k)
 			}
-			return false
 		}
 	}
-	return false
+	return data
 }
 
-//func anagram(m *map[string]struct{}, word []rune) []string {
-//
-//	return nil
-//}
-//
-//func FindAnagram(raw []string) *map[string][]string {
-//	tmp := toMap(raw)
-//}
+// Приводим все в единое состояние
+func getSiglegram(raw []string) *map[string]string {
+	abcmap := make(map[string]string, len(raw))
+	for i := range raw {
+		// Все слова должны быть приведены к нижнему регистру.
+		raw[i] = strings.ToLower(raw[i])
+		// Так же сортируем буквы по алфавиту для дальнейшего использования в качестве ключа
+		abcmap[raw[i]] = string(Sort([]rune(raw[i])))
+	}
+	return &abcmap
+}
+
+func makeAnagram(rawsigle *map[string]string) *map[string][]string {
+	mediaresult := make(map[string][]string)
+	result := make(map[string][]string)
+	for k, v := range *rawsigle {
+		mediaresult[v] = append(mediaresult[v], k)
+	}
+	for _, v := range mediaresult {
+		// Множества из одного элемента не должны попасть в результат.
+		if len(v) > 1 {
+			//Sort(v)
+			//Массив должен быть отсортирован по возрастанию.
+			result[v[0]] = v[1:]
+		}
+	}
+	return &result
+}
+
+// FindAnagram находит все анаграммы и возвращает результат
+func FindAnagram(raw []string) *map[string][]string {
+	rawsingle := getSiglegram(raw)
+	anag := makeAnagram(rawsingle)
+	return anag
+}
