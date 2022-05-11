8af815cda8ba312231317392ec22932f7a43389f
csamuro <3danger@mail,ru>
2022-05-11 13:32:46 +0300

    05 anagram: v003 done add comment

--- 05_FindAnagram/pkg/anagram/anagram.go
+++ 05_FindAnagram/pkg/anagram/anagram.go
@@ -21,32 +21,37 @@ func Sort[T string | rune](data []T) *[]T {
 
 // Приводим все в единое состояние
 func getSinglegram(raw []string) (*map[string]string, map[string]string) {
-	abcmap := make(map[string]string, len(raw))
-	first := make(map[string]string)
+	var abcMap = make(map[string]string, len(raw))
+	var firstKey = make(map[string]string)
 	for i := range raw {
 		// Все слова должны быть приведены к нижнему регистру.
 		raw[i] = strings.ToLower(raw[i])
 		abc := string(*Sort([]rune(raw[i])))
-		if _, exist := first[abc]; !exist {
-			first[abc] = raw[i]
+		if _, exist := firstKey[abc]; !exist {
+			/*
+				Сохраняем в мапу первое слово своей группы,
+				из которой потом будем извлекать значения
+				которые будут в качестве ключа для основной мапы
+			*/
+			firstKey[abc] = raw[i]
 		} else {
 			// Так же сортируем буквы по алфавиту для дальнейшего использования в качестве ключа
-			abcmap[raw[i]] = abc
+			abcMap[raw[i]] = abc
 		}
 	}
-	return &abcmap, first
+	return &abcMap, firstKey
 }
 
-func makeAnagram(rawsigle *map[string]string, first map[string]string) *map[string]*[]string {
-	mediaresult := make(map[string]*[]string)
+func makeAnagram(rawSingle *map[string]string, first map[string]string) *map[string]*[]string {
+	mediaResult := make(map[string]*[]string)
 	result := make(map[string]*[]string)
-	for k, v := range *rawsigle {
-		if _, ok := mediaresult[v]; !ok {
-			mediaresult[v] = new([]string)
+	for k, v := range *rawSingle {
+		if _, ok := mediaResult[v]; !ok {
+			mediaResult[v] = new([]string)
 		}
-		*mediaresult[v] = append(*(mediaresult[v]), k)
+		*mediaResult[v] = append(*(mediaResult[v]), k)
 	}
-	for a, v := range mediaresult {
+	for a, v := range mediaResult {
 		// Множества из одного элемента не должны попасть в результат.
 		if len(*v) > 1 {
 			//Массив должен быть отсортирован по возрастанию.
@@ -63,6 +68,6 @@ func makeAnagram(rawsigle *map[string]string, first map[string]string) *map[stri
 // Ключ - первое встретившееся в словаре слово из множества.
 // Значение - ссылка на массив, каждый элемент которого, слово из множества
 func FindAnagram(raw *[]string) *map[string]*[]string {
-	rawsingle, first := getSinglegram(*raw)
-	return makeAnagram(rawsingle, first)
+	rawSingle, firstKey := getSinglegram(*raw)
+	return makeAnagram(rawSingle, firstKey)
 }
