308a34671f4539ed1fb500dc7ff40a0e7f2345ee
csamuro <3danger@mail,ru>
2022-05-16 05:32:53 +0300

    08 shell kill review

--- 09_InteractionWithTheOS/shell/commands/builtins/ps.go
+++ 09_InteractionWithTheOS/shell/commands/builtins/ps.go
@@ -1,8 +1,12 @@
 package builtins
 
 import (
+	"errors"
+	"fmt"
+	ps "github.com/mitchellh/go-ps"
 	"microshell/shell/commands/common"
 	"sync"
+	"syscall"
 )
 
 type Ps struct {
@@ -10,7 +14,21 @@ type Ps struct {
 }
 
 func (p Ps) Run(group *sync.WaitGroup) (ok error) {
+	var (
+		proceses []ps.Process
+	)
+	if proceses, ok = ps.Processes(); ok != nil {
+		return errors.New("ps: " + ok.Error())
+	}
 
+	if _, ok = syscall.Write(p.Writer(), []byte(fmt.Sprintf("pid\tproc\n"))); ok != nil {
+		return errors.New("ps: " + ok.Error())
+	}
+	for _, proc := range proceses {
+		if _, ok = syscall.Write(p.Writer(), []byte(fmt.Sprintf("%d\t%s\n", proc.Pid(), proc.Executable()))); ok != nil {
+			return errors.New("ps: " + ok.Error())
+		}
+	}
 	group.Done()
 	p.CloseFds()
 	return nil
